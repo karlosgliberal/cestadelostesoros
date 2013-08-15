319c1364bbc1b7907d3ee8b0fbe252de54cce03d
karlos g liberal <karlosgliberal@gmail.com>
2013-08-15 09:58:00 +0200

    ejemplo parallax background

--- js/app.js
+++ js/app.js
@@ -1,6 +1,38 @@
 $(document).ready(function(){
 
-  $(window).stellar();
+  $(window).stellar({
+     // Set scrolling to be in either one or both directions
+  horizontalScrolling: false,
+    verticalScrolling: true,
+    
+    // Set the global alignment offsets
+    horizontalOffset: 0,
+    verticalOffset: 0,
+    
+    // Refreshes parallax content on window load and resize
+    responsive: true,
+    
+    // Select which property is used to calculate scroll.
+    // Choose 'scroll', 'position', 'margin' or 'transform',
+    // or write your own 'scrollProperty' plugin.
+    scrollProperty: 'scroll',
+    
+    // Select which property is used to position elements.
+    // Choose between 'position' or 'transform',
+    // or write your own 'positionProperty' plugin.
+    positionProperty: 'position',
+    
+    // Enable or disable the two types of parallax
+    parallaxBackgrounds: true,
+    parallaxElements: true,
+    
+    // Hide parallax elements that move outside the viewport
+    hideDistantElements: true,
+    
+    // Customise how elements are shown and hidden
+    hideElement: function($elem) { $elem.hide(); },
+    showElement: function($elem) { $elem.show(); }
+    });
 
   $('ul.nav a').smoothScroll({
       afterScroll: function() {
